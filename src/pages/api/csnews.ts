42369944571f1179f44d745104d76e5b5a9f8c7d
GMkonan <guilhemont@hotmail.com>
2023-08-23 08:02:57 -0300

    Add match preview details and improve text

--- src/pages/api/csnews.ts
+++ src/pages/api/csnews.ts
@@ -1,21 +1,33 @@
 import dayjs from "dayjs";
-import HLTV, { type EventPreview } from "hltv";
+import HLTV, { type MatchPreview, type EventPreview } from "hltv";
 import type { NextApiRequest, NextApiResponse } from "next";
 import { env } from "~/env.mjs";
 import { verifySignature } from "@upstash/qstash/nextjs";
 
-const sendToDiscord = async (content: EventPreview[]) => {
+const sendToDiscord = async (
+  content: EventPreview[],
+  matchDetails: MatchPreview[]
+) => {
   // seems like the closest event is always the 0 index (thank you hltv)
   const formmatedFirstEvent = {
     title: content[0]?.name,
-    date: content[0]?.dateStart,
+    dateStart: content[0]?.dateStart,
+    dateEnd: content[0]?.dateEnd,
+    team1: matchDetails[0]?.team1?.name || "",
+    team2: matchDetails[0]?.team2?.name || "",
+    gameFormat: matchDetails[0]?.format || "",
   };
 
-  const finalContent = `**GRITA CHELO!** Nossa grande FURIA vai estar na **${
+  // array with different stuff?
+  const finalContent = `**Compra 2 AWP abre a live que hoje tem clutch do fallen** Nossa FURIA vai estar na **${
     formmatedFirstEvent.title || ""
   }** Data: **${
-    dayjs(formmatedFirstEvent.date || 0).format("DD/MM/YYYY") || ""
-  }** Vamo la apoiar o fall... furia?`;
+    dayjs(formmatedFirstEvent.dateStart || 0).format("DD/MM/YYYY") || ""
+  }** ate **${dayjs(formmatedFirstEvent.dateEnd || 0).format("DD/MM/YYYY")}** \n
+  Jogo: **${formmatedFirstEvent.team1} VS ${formmatedFirstEvent.team2} ${
+    formmatedFirstEvent.gameFormat
+  }**
+  Vamo la apoiar o fall... furia?`;
 
   return fetch(env.DISCORD_WEBHOOK, {
     method: "post",
@@ -37,7 +49,12 @@ const csNews = async (req: NextApiRequest, res: NextApiResponse) => {
 
   const result = await HLTV.getEvents({ attendingTeamIds: TeamIds });
 
-  await sendToDiscord(result);
+  const matchDetails = await HLTV.getMatches({
+    eventIds: [result[0]?.id as number],
+    teamIds: TeamIds,
+  });
+
+  await sendToDiscord(result, matchDetails);
 
   return res.status(200).json(result);
 };
